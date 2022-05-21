067770457d903b5844bd09138b4a3ca7c1e759c7
KxmischesDomi <kxmischesdomi@gmail.com>
2022-05-21 17:13:41 +0200

    Block recipe book toggle with space and when searching

---
+++ src/main/java/de/kxmischesdomi/betterrecipebook/mixin/AbstractButtonMixin.java
@@ -0,0 +1,29 @@
+package de.kxmischesdomi.betterrecipebook.mixin;
+
+import net.minecraft.client.gui.components.AbstractButton;
+import net.minecraft.client.gui.components.ImageButton;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @author KxmischesDomi | https://github.com/kxmischesdomi
+ * @since 1.0
+ */
+@Mixin(AbstractButton.class)
+public class AbstractButtonMixin {
+
+	@Inject(method = "keyPressed", cancellable = true, at = @At("HEAD"))
+	public void keyPressedMixin(int i, int j, int k, CallbackInfoReturnable<Boolean> cir) {
+		if (i == 32) {
+			if (((Object) this) instanceof ImageButton imageButton) {
+				if (imageButton.resourceLocation.toString().equals("minecraft:textures/gui/recipe_button.png")) {
+					// Block toggling with space bar
+					cir.setReturnValue(true);
+				}
+			}
+		}
+	}
+
+}
--- src/main/java/de/kxmischesdomi/betterrecipebook/mixin/ScreenMixin.java
+++ src/main/java/de/kxmischesdomi/betterrecipebook/mixin/ScreenMixin.java
@@ -1,9 +1,12 @@
 package de.kxmischesdomi.betterrecipebook.mixin;
 
 import de.kxmischesdomi.betterrecipebook.BetterRecipeBookMod;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.Widget;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,13 +25,22 @@ public class ScreenMixin {
 
 	@Shadow @Final private List<Widget> renderables;
 
+	@Shadow @Final private List<GuiEventListener> children;
+
 	@Inject(method = "keyPressed", at = @At("TAIL"), cancellable = true)
 	public void keyPressedMixin(int i, int j, int k, CallbackInfoReturnable<Boolean> cir) {
 		if (BetterRecipeBookMod.recipeBookKeyBind.matches(i, j)) {
-
+			for (GuiEventListener child : children) {
+				if (child instanceof RecipeBookComponent recipeBookComponent) {
+					if (recipeBookComponent.searchBox.isFocused()) {
+						return;
+					}
+				}
+			}
 			for (Widget renderable : renderables) {
 				if (renderable instanceof ImageButton imageButton) {
 					if (imageButton.resourceLocation.toString().equals("minecraft:textures/gui/recipe_button.png")) {
+						imageButton.playDownSound(Minecraft.getInstance().getSoundManager());
 						imageButton.onPress();
 						break;
 					}
