4d31b5d6b55ba647d4ef7e6e796fb7d6fb861728
KxmischesDomi <kxmischesdomi@gmail.com>
2022-05-21 22:53:42 +0200

    Fix last recipe refill when other button is focused

--- src/main/java/de/kxmischesdomi/betterrecipebook/mixin/keybind/AbstractButtonMixin.java
+++ src/main/java/de/kxmischesdomi/betterrecipebook/mixin/keybind/AbstractButtonMixin.java
@@ -1,7 +1,11 @@
 package de.kxmischesdomi.betterrecipebook.mixin.keybind;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.components.ImageButton;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,8 +21,19 @@ public class AbstractButtonMixin {
 	@Inject(method = "keyPressed", cancellable = true, at = @At("HEAD"))
 	public void keyPressedMixin(int i, int j, int k, CallbackInfoReturnable<Boolean> cir) {
 		if (i == 32) {
+
+			Screen screen = Minecraft.getInstance().screen;
+			if (screen != null) {
+				for (GuiEventListener child : screen.children()) {
+					if (child instanceof RecipeBookComponent recipeBookComponent) {
+						recipeBookComponent.keyPressed(i, j, k);
+					}
+				}
+			}
+
 			if (((Object) this) instanceof ImageButton imageButton) {
 				if (imageButton.resourceLocation.toString().equals("minecraft:textures/gui/recipe_button.png")) {
+
 					// Block toggling with space bar
 					cir.setReturnValue(true);
 				}
